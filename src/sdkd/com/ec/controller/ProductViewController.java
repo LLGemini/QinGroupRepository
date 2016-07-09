033fb84c4574cae83394132559612d73eb977dd2
LLGemini <437313761@qq.com>
2016-07-09 09:08:34 +0800

    ljx 2016/7/9 第一次提交 web分页

--- src/sdkd/com/ec/controller/ProductViewController.java
+++ src/sdkd/com/ec/controller/ProductViewController.java
@@ -1,7 +1,9 @@
 package sdkd.com.ec.controller;
 
+import sdkd.com.ec.dao.impl.EbProductCategoryDao;
 import sdkd.com.ec.dao.impl.EbProductDao;
 import sdkd.com.ec.model.EbProduct;
+import sdkd.com.ec.model.EbProductCategory;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by Flower on 2016/7/7.
@@ -22,12 +25,15 @@ public class ProductViewController extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("utf8");
         response.setCharacterEncoding("utf-8");
+
         EbProductDao ebProductDao = new EbProductDao();
         String ep_id = request.getParameter("p_id");
-
         EbProduct product = ebProductDao.getProductDetail(ep_id);
-
         request.setAttribute("product", product);
+
+        EbProductCategoryDao epcd = new EbProductCategoryDao();
+        List<EbProductCategory> epc_list = epcd.getCategories();
+        request.setAttribute("category_list",epc_list);
 
         request.getRequestDispatcher("/product-view.jsp").forward(request,response);
 
